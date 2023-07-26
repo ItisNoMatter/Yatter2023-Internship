edf3fa8a7347dbc5b21f94ebf7b75acd35d3e789
iNoma <702sunnylife@gmail.com>
2023-07-26 12:38:31 +0900

    2#2まで完了

--- Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/MainActivity.kt
+++ Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.dmm.bootcamp.yatter2023
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,25 +8,28 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import com.dmm.bootcamp.yatter2023.ui.theme.Yatter2023Theme
+import com.dmm.bootcamp.yatter2023.ui.timeline.PublicTimelineActivity
 
 class MainActivity : AppCompatActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContent {
-      Yatter2023Theme {
-        // A surface container using the 'background' color from the theme
-        Surface(
-          modifier = Modifier.fillMaxSize(),
-          color = MaterialTheme.colors.background,
-        ) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Yatter2023Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background,
+                ) {
+                }
+            }
         }
-      }
-    }
+        startActivity(PublicTimelineActivity.newIntent(this))
+        finish()
 
-    val content: View = findViewById(android.R.id.content)
-    content.viewTreeObserver.addOnPreDrawListener { // Check if the initial data is ready.
-      false
+//    val content: View = findViewById(android.R.id.content)
+//    content.viewTreeObserver.addOnPreDrawListener { // Check if the initial data is ready.
+//      false
+//    }
     }
-  }
 }
